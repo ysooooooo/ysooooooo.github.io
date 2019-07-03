653f3849a54e208975938bc6a6eabbdfc75d06ad
ysooooooo <pys2210@hanmail.net>
2019-07-03 13:49:46 +0900

    daum

---
+++ daum/js/ui.js
@@ -0,0 +1,69 @@
+window.onload = function(){
+
+    //etcList의 각 메뉴를 클릭하면 그 메뉴를 보여주기
+    etcList();
+    function etcList(){
+        var etcBtn = document.querySelector(".etclist > .menu2 > ul");
+        var headEls = document.querySelectorAll(".head_tit > h3");
+        var titleEls = etcBtn.querySelectorAll("li");
+        var menuEls = document.querySelectorAll(".menu_list > div")
+
+        headEls[0].classList.add("on"); //기본값으로 첫번째 메뉴를 보여준다
+        titleEls[0].classList.add("on");
+        menuEls[0].classList.add("on");
+
+        etcBtn.addEventListener("click", function(e){
+            e.preventDefault();
+
+            for(var i=0;i<titleEls.length;i++){
+                if(titleEls[i].classList.contains("on")){
+                    headEls[i].classList.remove("on");
+                    titleEls[i].classList.remove("on");
+                    menuEls[i].classList.remove("on");
+                }
+            }
+            for(var j=0;j<titleEls.length;j++){
+                if(e.target.parentNode === titleEls[j]){
+                    headEls[j].classList.add("on");
+                    titleEls[j].classList.add("on");
+                    menuEls[j].classList.add("on");
+                }
+            }
+        });
+    }
+    
+    realTimeIssue();
+    function realTimeIssue(){
+        var hotIssueBtn = document.querySelector(".hotissue > .menu2 > ul");
+        var titleEls = hotIssueBtn.querySelectorAll("li");
+        var menuEls = document.querySelectorAll(".issue_area")
+
+        titleEls[0].classList.add("on"); //기본값으로 첫번째 메뉴를 보여준다
+        menuEls[0].classList.add("on");
+
+        hotIssueBtn.addEventListener("click", function(e){
+            e.preventDefault();
+
+            for(var i=0;i<titleEls.length;i++){
+                if(titleEls[i].classList.contains("on")){
+                    titleEls[i].classList.remove("on");
+                    menuEls[i].classList.remove("on");
+                }
+            }
+            for(var j=0;j<titleEls.length;j++){
+                if(e.target.parentNode === titleEls[j]){
+                    titleEls[j].classList.add("on");
+                    menuEls[j].classList.add("on");
+                }
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+}
